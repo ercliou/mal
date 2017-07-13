3c2b703d9c3629b86c7db5cf2c80fe7686aef959
Eric Liou <eric.liou@nubank.com.br>
2017-07-13 19:23:45 -0300

    Step 0, repl

--- clojure/src/mal/step0_repl.clj
+++ clojure/src/mal/step0_repl.clj
@@ -1,4 +1,17 @@
 (ns mal.step0-repl
   (:gen-class))
 
-(defn -main [] (println "abc"))
+(defn e-read [s] s)
+(defn e-eval [s] s)
+(defn e-print [s] s)
+
+(defn rep [s]
+  (-> s e-read e-eval e-print))
+
+(defn -main []
+  (while true
+    (print "user> ")
+    (flush)
+    (-> (read-line)
+        rep
+        println)))
